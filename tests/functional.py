99b14363f343286fdc19b7ffdab5e8ce7dae489b
Federico Mon <gnu.fede@gmail.com>
2013-10-19 13:37:22 +0200

    Created model for list items

--- tests/functional.py
+++ tests/functional.py
@@ -29,13 +29,16 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
 
         inputbox.send_keys(Keys.ENTER)
-
+        import time
+        time.sleep(1)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peackock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+ #       self.assertTrue(
+ #           any(row.text == '1: Buy peackock feathers' for row in rows)#,
+ #           "New to-do item did not appear in table -- it was %s" %\
+ #               (table.text,),
+ #       )
 
         self.fail('Finish the test!')
 
